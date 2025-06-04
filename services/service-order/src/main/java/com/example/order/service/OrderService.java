bf64da3ced7032cd026340bdb6b9339239286c32
xwk <1376427798@qq.com>
2025-06-05 00:03:19 +0800

    Revert "day01"
    This reverts commit 419031e0f41bd9972a934290bc093fc36154b81a.

--- services/service-order/src/main/java/com/example/order/service/OrderService.java
+++
@@ -1,6 +0,0 @@
-package com.example.order.service;
-
-import com.example.model.bean.Order;
-public interface OrderService {
-    Order getOrder(Long userId, Long productId);
-}
--- services/service-product/src/main/java/com/example/product/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.example.product.service;
-
-import com.example.model.bean.Product;
-
-public interface ProductService {
-    Product getProduct(Long productId);
-}
